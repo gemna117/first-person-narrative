37ab271630377d4db226baaf62f6132b86bbec80
gemna117 <megan.alonzo@loop.colum.edu>
2020-03-16 18:01:08 -0700

    door animation

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Door : InteractiveObject
+{
+    private Animator animator;
+    private bool isOpen;
+
+    public Door()
+    {
+        displaytext = nameof(Door);
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public override void InteractWith()
+    {
+        if (isOpen)
+        {
+        base.InteractWith();
+        animator.SetBool("shouldOpen", true);
+            displaytext = string.Empty;
+            isOpen = true;
+
+        }
+    }
+}
