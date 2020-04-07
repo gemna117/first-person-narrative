74234cb8393d26b95a06055ffb0995199f38c4f8
gemna117 <megan.alonzo@loop.colum.edu>
2020-04-07 15:53:41 -0700

    what i have for milestone 3

--- Assets/Scripts/TitleMenu.cs
+++ Assets/Scripts/TitleMenu.cs
@@ -6,9 +6,25 @@ public class TitleMenu : MonoBehaviour
     [SerializeField]
     private string gameSceneName;
 
+    [SerializeField]
+    private GameObject creditsMenuPanel;
+
+    [SerializeField]
+    private GameObject titleMenuPanel;
+
     public void loadgamescene()
     {
         SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void showcredits()
+    {
+        creditsMenuPanel.SetActive(true);
+    }
+
+    public void showtitle()
+    {
+        titleMenuPanel.SetActive(true);
     }
 
     public void exitgame()
